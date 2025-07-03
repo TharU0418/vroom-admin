1886769b3752c3e6795c9c6c1d4647de3e466149
Tharusha Dilshan <tharushadil11@gmail.com>
2025-07-03 12:56:00 +0530

    Update index.ts

--- pages/api/fetch/index.ts
+++ pages/api/fetch/index.ts
@@ -1,33 +1,33 @@
 // fetch.ts
-import Rentrequests from "../../../models/rentrequests";
-import Hirerequest from "../../../models/hirerequests";
+//import Rentrequests from "../../../models/rentrequests";
+//import Hirerequest from "../../../models/hirerequests";
 
 export async function getOverviewData() {
   //await dbConnect();
 
-  const totalRequests = await Rentrequests.countDocuments({});
-  const pendingRequests = await Rentrequests.countDocuments({ availability: 'pending' });
-  //const acceptedRequests = await Rentrequests.countDocuments({ availability: 'accept' });
-  const totalHRequests = await Hirerequest.countDocuments({});
-  const pendingHRequests = await Hirerequest.countDocuments({availability: 'pending' });
+  // const totalRequests = await Rentrequests.countDocuments({});
+  // const pendingRequests = await Rentrequests.countDocuments({ availability: 'pending' });
+  // //const acceptedRequests = await Rentrequests.countDocuments({ availability: 'accept' });
+  // const totalHRequests = await Hirerequest.countDocuments({});
+  // const pendingHRequests = await Hirerequest.countDocuments({availability: 'pending' });
 
 
   // Growth rate is mocked for now – you'd calculate based on date ranges or previous period
   return {
     totalRequests: {
-      value: totalRequests,
+      value: 12,
       //growthRate: 5.2, // mock
     },
     pendingRequests: {
-      value: pendingRequests,
+      value: 1,
       //growthRate: -3.1, // mock
     },
     acceptedRequests: {
-      value: totalHRequests,
+      value: 5,
       growthRate: 2.7, // mock
     },
     pendingHRequests: {
-      value: pendingHRequests,
+      value: 1,
       //growthRate: 2.7, // mock
     },
   };
