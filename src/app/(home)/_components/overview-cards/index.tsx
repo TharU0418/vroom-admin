bf7dd448e5faf965b370a311f1a43c8e0973e701
Tharusha Dilshan <tharushadil11@gmail.com>
2025-07-03 13:04:26 +0530

    Update index.tsx

--- src/app/(home)/_components/overview-cards/index.tsx
+++ src/app/(home)/_components/overview-cards/index.tsx
@@ -1,55 +1,3 @@
-// import { compactFormat } from "@/lib/format-number";
-// import { getOverviewData } from "../../fetch";
-// import { OverviewCard } from "./card";
-// import * as icons from "./icons";
-
-// export async function OverviewCardsGroup() {
-//   const { views, profit, products, users } = await getOverviewData();
-
-//   return (
-//     <div className="grid gap-4 sm:grid-cols-2 sm:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-//       <OverviewCard
-//         label="Total Views"
-//         data={{
-//           ...views,
-//           value: compactFormat(views.value),
-//         }}
-//         Icon={icons.Views}
-//       />
-
-//       <OverviewCard
-//         label="Total Profit"
-//         data={{
-//           ...profit,
-//           value: "$" + compactFormat(profit.value),
-//         }}
-//         Icon={icons.Profit}
-//       />
-
-//       <OverviewCard
-//         label="Total Products"
-//         data={{
-//           ...products,
-//           value: compactFormat(products.value),
-//         }}
-//         Icon={icons.Product}
-//       />
-
-//       <OverviewCard
-//         label="Total Users"
-//         data={{
-//           ...users,
-//           value: compactFormat(users.value),
-//         }}
-//         Icon={icons.Users}
-//       />
-//     </div>
-//   );
-// }
-
-
-// index.tsx
-
 import { compactFormat } from "@/lib/format-number";
 import { OverviewCard } from "./card";
 import * as icons from "./icons";
@@ -66,39 +14,45 @@ export async function OverviewCardsGroup() {
   return (
     <div className="grid gap-4 sm:grid-cols-2 sm:gap-6 xl:grid-cols-4 2xl:gap-7.5">
       <OverviewCard
-        label="Total Car Requests"
-        data={{
-          ...totalRequests,
-          value: compactFormat(totalRequests.value),
-        }}
-        Icon={icons.Users}
-      />
+  label="Total Car Requests"
+  data={{
+    ...totalRequests,
+    value: compactFormat(totalRequests.value),
+    growthRate: totalRequests.growthRate, // ensure growthRate is explicitly passed
+  }}
+  Icon={icons.Users}
+/>
 
-      <OverviewCard
-        label="Pending Car Requests"
-        data={{
-          ...pendingRequests,
-          value: compactFormat(pendingRequests.value),
-        }}
-        Icon={icons.Product}
-      />
+<OverviewCard
+  label="Pending Car Requests"
+  data={{
+    ...pendingRequests,
+    value: compactFormat(pendingRequests.value),
+    growthRate: pendingRequests.growthRate,
+  }}
+  Icon={icons.Product}
+/>
 
-      <OverviewCard
-        label="Total Hire Requests"
-        data={{
-          ...acceptedRequests,
-          value: compactFormat(acceptedRequests.value),
-        }}
-        Icon={icons.Views}
-      />
-      <OverviewCard
-        label="Pending Hire Requests"
-        data={{
-          ...pendingHRequests,
-          value: compactFormat(pendingHRequests.value),
-        }}
-        Icon={icons.Views}
-      />
+<OverviewCard
+  label="Total Hire Requests"
+  data={{
+    ...acceptedRequests,
+    value: compactFormat(acceptedRequests.value),
+    growthRate: acceptedRequests.growthRate,
+  }}
+  Icon={icons.Views}
+/>
+
+<OverviewCard
+  label="Pending Hire Requests"
+  data={{
+    ...pendingHRequests,
+    value: compactFormat(pendingHRequests.value),
+    growthRate: pendingHRequests.growthRate,
+  }}
+  Icon={icons.Views}
+/>
+
       
     </div>
   );
