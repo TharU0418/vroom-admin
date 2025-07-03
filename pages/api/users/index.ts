902cfe6ceb32870432b64476c647ef1b8a004623
Tharusha Dilshan <tharushadil11@gmail.com>
2025-07-03 12:58:55 +0530

    Update index.ts

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,16 +1,19 @@
 // pages/api/users/index.ts
 //import { dbConnect } from '@/lib/dbConnect';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import User from '../../../models/User';
+//import User from '../../../models/User';
+
+
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   //await dbConnect();
 
+
   if (req.method === 'GET') {
     try {
-      const users = await User.find({}, 'firstName lastName email mobileNumber location'); // Select only needed fields
-      res.status(200).json(users);
+    //  const users = await User.find({}, 'firstName lastName email mobileNumber location'); // Select only needed fields
+     // res.status(200).json(users);
     } catch (error) {
       console.error('Failed to fetch users:', error);
       res.status(500).json({ message: 'Server error while fetching users' });
