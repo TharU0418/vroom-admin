3f30ba09f999d077e4c0cbbd21d0e0769ba6b58c
Tharusha Dilshan <tharushadil11@gmail.com>
2025-07-04 10:19:18 +0530

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,33 +1,35 @@
-import { PaymentsOverview } from "@/components/Charts/payments-overview";
-import { UsedDevices } from "@/components/Charts/used-devices";
-import { WeeksProfit } from "@/components/Charts/weeks-profit";
-import { TopChannels } from "@/components/Tables/top-channels";
-import { TopChannelsSkeleton } from "@/components/Tables/top-channels/skeleton";
-import { createTimeFrameExtractor } from "@/utils/timeframe-extractor";
-import { Suspense } from "react";
-import { ChatsCard } from "./_components/chats-card";
-import { OverviewCardsGroup } from "./_components/overview-cards";
-import { OverviewCardsSkeleton } from "./_components/overview-cards/skeleton";
-import { RegionLabels } from "./_components/region-labels";
+// import { PaymentsOverview } from "@/components/Charts/payments-overview";
+// import { UsedDevices } from "@/components/Charts/used-devices";
+// import { WeeksProfit } from "@/components/Charts/weeks-profit";
+// import { TopChannels } from "@/components/Tables/top-channels";
+// import { TopChannelsSkeleton } from "@/components/Tables/top-channels/skeleton";
+// import { createTimeFrameExtractor } from "@/utils/timeframe-extractor";
+// import { Suspense } from "react";
+// import { ChatsCard } from "./_components/chats-card";
+// import { OverviewCardsGroup } from "./_components/overview-cards";
+// import { OverviewCardsSkeleton } from "./_components/overview-cards/skeleton";
+// import { RegionLabels } from "./_components/region-labels";
 
-type PropsType = {
-  searchParams: Promise<{
-    selected_time_frame?: string;
-  }>;
-};
+// type PropsType = {
+//   searchParams: Promise<{
+//     selected_time_frame?: string;
+//   }>;
+// };
 
-export default async function Home({ searchParams }: PropsType) {
-  const { selected_time_frame } = await searchParams;
-  const extractTimeFrame = createTimeFrameExtractor(selected_time_frame);
+export default async function Home() {
+
+//export default async function Home({ searchParams }: PropsType) {
+  //const { selected_time_frame } = await searchParams;
+ // const extractTimeFrame = createTimeFrameExtractor(selected_time_frame);
 
   return (
     <>
-      <Suspense fallback={<OverviewCardsSkeleton />}>
+      {/* <Suspense fallback={<OverviewCardsSkeleton />}>
         <OverviewCardsGroup />
-      </Suspense>
+      </Suspense> */}
 
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
-        <PaymentsOverview
+       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
+      {/*  <PaymentsOverview
           className="col-span-12 xl:col-span-7"
           key={extractTimeFrame("payments_overview")}
           timeFrame={extractTimeFrame("payments_overview")?.split(":")[1]}
@@ -43,9 +45,9 @@ export default async function Home({ searchParams }: PropsType) {
           className="col-span-12 xl:col-span-5"
           key={extractTimeFrame("used_devices")}
           timeFrame={extractTimeFrame("used_devices")?.split(":")[1]}
-        />
+        /> */}
 
-        <RegionLabels />
+        {/* <RegionLabels />
 
         <div className="col-span-12 grid xl:col-span-8">
           <Suspense fallback={<TopChannelsSkeleton />}>
@@ -55,7 +57,7 @@ export default async function Home({ searchParams }: PropsType) {
 
         <Suspense fallback={null}>
           <ChatsCard />
-        </Suspense>
+        </Suspense> */}
       </div>
     </>
   );
